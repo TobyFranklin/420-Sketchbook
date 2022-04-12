6f73d4a0de5603c4c96d1973db6dd3970d508d7b
Toby Franklin <mst3kninja@gmail.com>
2022-04-12 04:21:08 -0400

    Week 13       Shaders 2: Electric Boogaloo

---
+++ ProceduralProject/Assets/Shaders/PostProcessing.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessing : MonoBehaviour
+{
+
+    public Shader shader;
+    private Material mat;
+
+    public Texture noiseTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = new Material(shader);
+
+        mat.SetTexture("_NoiseTex", noiseTexture);
+    }
+
+    // Update is called once per frame
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, mat);
+    }
+}
